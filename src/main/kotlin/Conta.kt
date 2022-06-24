639e3ed76fad025fb036010b869bc693a931289e
IgorSMendes2001 <igor07082001@gmail.com>
2022-06-24 17:42:12 -0300

    Mais alterações

--- src/main/kotlin/Conta.kt
+++ src/main/kotlin/Conta.kt
@@ -4,15 +4,15 @@ class Conta {
     var saldo=0.0
 
     constructor()
-    fun depositar(conta:Conta,valor:Double){
-        conta.saldo+=valor
+    fun depositar(valor:Double){
+        this.saldo+=valor
         println("Depósito de $valor realizado com sucesso!")
     }
-    fun sacar(conta:Conta,valor: Double){
-        if(conta.saldo<=0){
+    fun sacar(valor: Double){
+        if(this.saldo<valor){
             println("Impossível Sacar,saldo insuficiente")
         }else{
-            conta.saldo-=valor
+            this.saldo-=valor
             println("Saque de $valor realizado com sucesso!")
         }
     }
@@ -25,4 +25,8 @@ fun main(){
     conta1.numeroConta=222
     conta1.saldo=2500.00
     println("Número da conta : ${conta1.numeroConta} , nome do titular: ${conta1.titular},e o seu saldo é: ${conta1.saldo}")
+    conta1.depositar(2000.0)
+    println(conta1.saldo)
+    conta1.sacar(1500.0)
+    println(conta1.saldo)
 }
